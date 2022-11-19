38a4d1d7edcb2bcfa702931f21b94ab3b2ba2125
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-19 15:22:14 +0530

    Initial Commit

---
+++ CollectionViewNumberSeries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewNumberSeries
+//
+//  Created by Bhumika Patel on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
